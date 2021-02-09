f312ff1c1b18f9a59ca63dd8fe433766b483b177
Ikenna Omekam <iomekam@github.com>
2021-02-09 01:54:51 -0500

    more redactions

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -9,13 +9,17 @@ REQUEST_HEADERS = %w[
 
 RESPONSE_HEADERS = %w[Set-Cookie].freeze
 
+KEYS = %w[idToken refreshToken].freeze
+
+
 VCR.configure do |config|
   config.cassette_library_dir = "spec/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.default_cassette_options = {
-    match_requests_on: [:method,
-                        VCR.request_matchers.uri_without_param(:key)]
+    match_requests_on: [:method, :body, :uri,
+                        VCR.request_matchers.uri_without_param(:key)],
+    record: :once
   }
 
   config.filter_sensitive_data("<redacted-request>") do |interaction|
@@ -33,5 +37,19 @@ VCR.configure do |config|
   config.filter_sensitive_data("<redacted-key>") do |interaction|
     start_index = interaction.request.uri.index("key=")
     interaction.request.uri[(start_index + "key=".size)..interaction.request.uri.size]
+  end
+
+  KEYS.each do |key|
+    config.filter_sensitive_data("<redacted-key>") do |interaction|
+      payload = JSON.parse(interaction.request.body)
+      payload[key] if payload.key? key
+    end
+  end
+
+  KEYS.each do |key|
+    config.filter_sensitive_data("<redacted-key>") do |interaction|
+      payload = JSON.parse(interaction.response.body)
+      payload[key] if payload.key? key
+    end
   end
 end
